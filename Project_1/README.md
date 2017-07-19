ec7dba5f5103bf1fccd1244f491dbb2062855d8f
Linwood Lassiter <linwoodlassiter@Linwoods-MBP.PK5001Z>
2017-07-18 21:26:29 -0400

    project 1 updates

--- Project_1/README.md
+++ Project_1/README.md
@@ -1,19 +1,20 @@
 Project 1 -- Blackjack MVP
 
-  Setup
-    • Setup background - Green Playing table
+  Setup - up generateComputer
+done    • Setup background - Green Playing table
     • Show deck of cards on playing table
-    • Set array of 52 card objects (suite, value)
-    • Setup Player and Computer/Dealer objects
-    • Random deal (pop or count = 52) form card array updating html  to display dealt cards
+done    • Set array of 52 card objects (suite, value)
+????    • Setup Player and Computer/Dealer objects
+done    • Random deal (pop or count = 52) form card array updating html  to display dealt cards
   Player/Dealer(Computer)
-    • bankroll -= $10.00 ante on start (ante or end button)
-    • update pot += ante or bet
-    • 2 cards dealt to each player
-    • calculate score for dealer/player
-      Player Hit (on hit bet add score)
-          •Bet if bankroll >= 0 else EOG (bet/stay button)
-          •player bet amount (bankroll -= bet)
+done    • bankroll -= bet on start (ante or end button)
+done    • update pot += ante or bet
+done    • 2 cards dealt to each player
+done    • calculate score for dealer/player
+      Player Hit (add score dealer/player)
+done          •Bet if bankroll >= 0 else EOG (bet/stay button)
+done          •player bet amount (bankroll -= bet)
+          •dealer draws till >= 17
           •player takes another card
           •Player calc total > 21 busted EORound
           •Dealer/Computer may take card or stay
@@ -29,8 +30,14 @@ Project 1 -- Blackjack MVP
           •compare dealer card total with player
 
     Strech Goals
-      Annomate dealing of cards off the deck
-      Play with 4 decks or continous cards
-      Player has no $ must Stay! win/loose pot ?
+      •Annomate dealing of cards off the deck
+      •Play with 4 decks or continous cards
+      •Player has no $ must Stay! win/loose pot ?
+      •algorithm to implement betting strategy
+      •insurance - side bet 2:1 payout (dealer up card is an ace)
+      •split play
+      •Aces are 1 or 11 (11 for now
+      •
+
 
 https://ga-students.slack.com/files/kristyn/F6AA69GCS/mvps_that_we_discussed_for_project_1.js
--- Project_1/scripts/app.js
+++ Project_1/scripts/app.js
@@ -1,12 +1,118 @@
 $(()=>{
 console.log('hooked up');
 
+  let playerBankRoll = 500;
+  let thePot = 0;
+
+  class Card {
+    constructor(suit, value) {
+      this.suit = suit;
+      this.value = value;
+    }
+  };
+
+  class FaceCard extends Card {
+    constructor(suit, value, face) {
+      super(suit, value);
+      this.face = face;
+    }
+  }
+
+  const cards = {
+    deck: [],
+    dealerCards: [],
+    playerCards: []
+  };
 
 
+  //set cards in a deck
+  const createCards = () => {
+    const suit = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+
+    for ( let i = 0; i < suit.length; i++ ) {
+      //create Ace - 10
+      for (let j = 2; j <= 10; j++ ) {
+        cards.deck.push(new Card(suit[i], j));
+      }
+      //update Ace face
+      cards.deck.push(new FaceCard(suit[i], 11, 'Ace'));
+      cards.deck.push(new FaceCard(suit[i], 10, 'Jack'));
+      cards.deck.push(new FaceCard(suit[i], 10, 'Queen'));
+      cards.deck.push(new FaceCard(suit[i], 10, 'King'));
+    }
+  };
+
+  const dealCard = () => {
+    if (cards.deck.length > 0) {
+      const randomIndex = Math.floor((Math.random() * 53) + 1);
+      // console.log('randomIndex', cards.deck[randomIndex]);
+      return(cards.deck.splice(randomIndex, 1));
+    } else {
+      return(null);
+    }
+  };
+
+  const placeyourBet = (Bet) => {
+    console.log('cards.deck.length', cards.deck.length);
+
+    //test for dealCard return of null... nomore cards and EOG
+    //if cards.deck.length < 4 --> EOG.
+    //
+    cards.playerCards.push(dealCard());
+    cards.playerCards.push(dealCard());
+    cards.dealerCards.push(dealCard());
+    cards.dealerCards.push(dealCard());
+    console.log('cards.deck.length', cards.deck.length);
+    //take the anti
+    playerBankRoll-=bet;
+    //no 2:3 nor 5:7 yet....
+    thePot+=bet;
+  };
+
+  const scoreTotal = (cards) => {
+      let totalScore = 0;
+      for ( let i = 0; i < cards.length; i++ ) {
+        console.log('cards[' + i + '].value', cards[i][0].value);
+        totalScore+=cards[i][0].value;
+      }
+      return(totalScore);
+  };
+
+  createCards();
+  //button ante up (2 cards each) get bet from input
+  //if bankroll <= 0 EOG
+  placeyourBet(10);
+
+  //Player draws till > 17  if dealer up card is < 6 or stay
+  // let hitme = false;
+  let playerScore = scoreTotal(cards.playerCards);
+  //player hit or stay......
+  //algorithm for hit or stay
 
 
+  let dealerScore = scoreTotal(cards.dealerCards);
+  //dealer draws until >= 17
+  while ( dealerScore < 17 ) {
+        cards.dealerCards.push(dealCard());
+  };
 
-
-
+  // //check cards
+  // for (let i = 0; i < cards.deck.length; i++) {
+  //   console.log(cards.deck[i]);
+  // }
+  console.log('cards.deck.length', cards.deck.length)
+  // console.log('cards.playerCards', cards.playerCards.length);
+  // console.log('FaceCard', cards.playerCards[0][0] instanceof FaceCard );
+  // console.log('FaceCard', cards.playerCards[1][0] instanceof FaceCard );
+  // console.log(cards.playerCards[0][0].value);
+  // console.log(cards.playerCards[1][0].value);
+  // console.log(cards.playerCards[0][0].face);
+  // console.log(cards.playerCards[1][0].face);
+  // console.log('cards.dealerCards', cards.dealerCards.length);
+  // console.log(cards.dealerCards[0][0].value);
+  // console.log(cards.dealerCards[1][0].value);
+  //
+  console.log('playerScore', playerScore);
+  console.log('dealerScore', dealerScore);
 
 });
